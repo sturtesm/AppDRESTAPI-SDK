6f162febfcfbbe288de1891a083083022f3926e5
gilbertsolorzanoFS <gilbert.solorzano@appdynamics.com>
2014-10-07 07:05:25 -0700

    Added missing java class

---
+++ src/org/appdynamics/appdrestapi/util/RequestSnapshots.java
@@ -0,0 +1,196 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.appdynamics.appdrestapi.util;
+
+/**
+ *
+ * @author gilbert.solorzano
+ */
+public class RequestSnapshots {
+    private String guids;
+    private String deepDeivePolicy;
+    private String applicationComponentIds;
+    private String applicationComponentNodeIds;
+    private String businessTransactionIds;
+    private String userExperience;
+    private String firstInChain;
+    private String needProps;
+    private String needExitCalls;
+    private String executionTimeInMillis;
+    private String sessionId;
+    private String userPrincipalId;
+    private String errorIds;
+    private String startingRequestIdEndingRequestId;
+    private String errorOccurred;
+    private String diagnosticSnapshot;
+    private String dataCollectorName;
+    private String dataCollectorValue;
+    private String dataCollectorType;
+    private String output;
+
+    public String getGuids() {
+        return guids;
+    }
+
+    public void setGuids(String guids) {
+        this.guids = guids;
+    }
+
+    public String getDeepDeivePolicy() {
+        return deepDeivePolicy;
+    }
+
+    public void setDeepDeivePolicy(String deepDeivePolicy) {
+        this.deepDeivePolicy = deepDeivePolicy;
+    }
+
+    public String getApplicationComponentIds() {
+        return applicationComponentIds;
+    }
+
+    public void setApplicationComponentIds(String applicationComponentIds) {
+        this.applicationComponentIds = applicationComponentIds;
+    }
+
+    public String getApplicationComponentNodeIds() {
+        return applicationComponentNodeIds;
+    }
+
+    public void setApplicationComponentNodeIds(String applicationComponentNodeIds) {
+        this.applicationComponentNodeIds = applicationComponentNodeIds;
+    }
+
+    public String getBusinessTransactionIds() {
+        return businessTransactionIds;
+    }
+
+    public void setBusinessTransactionIds(String businessTransactionIds) {
+        this.businessTransactionIds = businessTransactionIds;
+    }
+
+    public String getUserExperience() {
+        return userExperience;
+    }
+
+    public void setUserExperience(String userExperience) {
+        this.userExperience = userExperience;
+    }
+
+    public String getFirstInChain() {
+        return firstInChain;
+    }
+
+    public void setFirstInChain(String firstInChain) {
+        this.firstInChain = firstInChain;
+    }
+
+    public String getNeedProps() {
+        return needProps;
+    }
+
+    public void setNeedProps(String needProps) {
+        this.needProps = needProps;
+    }
+
+    public String getNeedExitCalls() {
+        return needExitCalls;
+    }
+
+    public void setNeedExitCalls(String needExitCalls) {
+        this.needExitCalls = needExitCalls;
+    }
+
+    public String getExecutionTimeInMillis() {
+        return executionTimeInMillis;
+    }
+
+    public void setExecutionTimeInMillis(String executionTimeInMillis) {
+        this.executionTimeInMillis = executionTimeInMillis;
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public String getUserPrincipalId() {
+        return userPrincipalId;
+    }
+
+    public void setUserPrincipalId(String userPrincipalId) {
+        this.userPrincipalId = userPrincipalId;
+    }
+
+    public String getErrorIds() {
+        return errorIds;
+    }
+
+    public void setErrorIds(String errorIds) {
+        this.errorIds = errorIds;
+    }
+
+    public String getStartingRequestIdEndingRequestId() {
+        return startingRequestIdEndingRequestId;
+    }
+
+    public void setStartingRequestIdEndingRequestId(String startingRequestIdEndingRequestId) {
+        this.startingRequestIdEndingRequestId = startingRequestIdEndingRequestId;
+    }
+
+    public String getErrorOccurred() {
+        return errorOccurred;
+    }
+
+    public void setErrorOccurred(String errorOccurred) {
+        this.errorOccurred = errorOccurred;
+    }
+
+    public String getDiagnosticSnapshot() {
+        return diagnosticSnapshot;
+    }
+
+    public void setDiagnosticSnapshot(String diagnosticSnapshot) {
+        this.diagnosticSnapshot = diagnosticSnapshot;
+    }
+
+    public String getDataCollectorName() {
+        return dataCollectorName;
+    }
+
+    public void setDataCollectorName(String dataCollectorName) {
+        this.dataCollectorName = dataCollectorName;
+    }
+
+    public String getDataCollectorValue() {
+        return dataCollectorValue;
+    }
+
+    public void setDataCollectorValue(String dataCollectorValue) {
+        this.dataCollectorValue = dataCollectorValue;
+    }
+
+    public String getDataCollectorType() {
+        return dataCollectorType;
+    }
+
+    public void setDataCollectorType(String dataCollectorType) {
+        this.dataCollectorType = dataCollectorType;
+    }
+
+    public String getOutput() {
+        return output;
+    }
+
+    public void setOutput(String output) {
+        this.output = output;
+    }
+    
+    
+    
+    
+}
