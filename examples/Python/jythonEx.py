98372672cc5d7447b1f0a11669f9c37cb2c842c1
gilbertsolorzanoFS <gilbert.solorzano@appdynamics.com>
2015-06-01 20:35:48 -0700

    Removed password entry

--- examples/Python/jythonEx.py
+++ examples/Python/jythonEx.py
@@ -5,6 +5,6 @@
 #import sys
 from org.appdynamics.appdrestapi import RESTAccess
 
-access = RESTAccess("controller3.demo.appdynamics.com","443",True,"demouser","apm13ad3r","customer1")
+access = RESTAccess("controller3.demo.appdynamics.com","443",True,"demouser1","xxxxxxxxx","customer1")
 print access.getApplications()
 print "Hello\n"
